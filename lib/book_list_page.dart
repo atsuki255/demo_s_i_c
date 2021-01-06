8e2b42b742708a1c6f3eaeca9409154ee3897e30
atsuki225 <atsuki443@gmail.com>
2021-01-06 23:11:45 +0900

    fix BookList

--- lib/book_list_page.dart
+++ lib/book_list_page.dart
@@ -12,20 +12,14 @@ class BookList extends StatelessWidget {
         // ignore: deprecated_member_use
         stream: Firestore.instance.collection('books').snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return new Text('Loading...');
-            default:
-              return new ListView(
-                // ignore: deprecated_member_use
-                children: snapshot.data.documents.map((DocumentSnapshot document) {
-                  return new ListTile(
-                    title: new Text(document['title']),
-                  );
-                }).toList(),
+          return ListView(
+            // ignore: deprecated_member_use
+            children: snapshot.data.documents.map((DocumentSnapshot document) {
+              return ListTile(
+                title: Text(document['title']),
               );
-          }
+            }).toList(),
+          );
         },
       ),
     );
