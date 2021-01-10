062af33dc681571ca81bf991681dbe2424dba084
atsuki225 <atsuki443@gmail.com>
2021-01-10 10:44:16 +0900

    Firebase Authを使用しての新規登録・ログイン

--- lib/signup/signup_model.dart
+++ lib/signup/signup_model.dart
@@ -1,39 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class SignUpModel extends ChangeNotifier {
-  String mail = "";
-  String password = "";
+  String mail = '';
+  String password = '';
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future signUp() async {
     if (mail.isEmpty) {
-      throw ("メールアドレスを入力してください！");
-      //バリデーション
+      throw ('メールアドレスを入力してください');
     }
 
     if (password.isEmpty) {
-      throw ("パスワードを入力してください！");
-      //バリデーション
+      throw ('パスワードを入力してください');
     }
 
-    //todo;
-    // ignore: deprecated_member_use
-    final FirebaseUser user = (await _auth.createUserWithEmailAndPassword(
+    // todo
+    final User user = (await _auth.createUserWithEmailAndPassword(
       email: mail,
       password: password,
     ))
         .user;
     final email = user.email;
 
-    // ignore: deprecated_member_use
-    Firestore.instance.collection('users').add(
+    FirebaseFirestore.instance.collection('users').add(
       {
         'email': email,
-        "createdAt": Timestamp.now(),
+        'createdAt': Timestamp.now(),
       },
     );
   }
